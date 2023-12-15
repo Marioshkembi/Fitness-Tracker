1ebd3a8f5566aaf10fffb7caa3676a64af4cb18e
Mario Shkembi <marioskm7@gmail.com>
2023-12-15 22:44:05 +0200

    Initial Commit

---
+++ Fitness Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fitness Tracker
+//
+//  Created by Mariglen Shkembi on 15/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fitness Tracker/Fitness_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_TrackerApp.swift
+//  Fitness Tracker
+//
+//  Created by Mariglen Shkembi on 15/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
